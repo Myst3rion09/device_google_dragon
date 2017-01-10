e3a901e537c695b072aa7f96064f27d2d215e329
beanstown106 <nbenis106@gmail.com>
2017-01-10 17:31:31 -0500

    dragon: add facelock props
    Change-Id: I55d279642230a5faec7faa71112febaa08b3ad29

--- device-slim.mk
+++ device-slim.mk
@@ -1,3 +1,10 @@
+# Facelock props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=2500 \
+    ro.facelock.rec_timeout=3500 \
+    ro.facelock.est_max_time=500
+
 # Mobile data provision prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.prov_mobiledata=false
