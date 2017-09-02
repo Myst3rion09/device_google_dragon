4f4647ad65237e3e9e5f8b6f2d718ceca352eeb4
beanstown106 <nbenis106@gmail.com>
2017-09-03 00:07:43 +0200

    dragon: add drm props

--- device-omni.mk
+++ device-omni.mk
@@ -1,2 +1,7 @@
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.com.widevine.cachesize=16777216
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/dragon/overlay-omni
