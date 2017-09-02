fd495e41becb0f27542ef2f891eec8702b90b482
beanstown106 <nbenis106@gmail.com>
2017-09-03 00:10:55 +0200

    dragon: add camera flash prop

--- device-omni.mk
+++ device-omni.mk
@@ -3,5 +3,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.com.widevine.cachesize=16777216
 
+# Camera flash
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.flash_off=0
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/dragon/overlay-omni
